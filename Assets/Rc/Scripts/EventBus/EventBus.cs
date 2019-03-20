33a4d62bc332421e687b7d966f9489b154d3ba48
rugnight <rugnight@gmail.com>
2019-03-21 08:11:16 +0900

    EventBusコメント追加

--- Assets/Rc/Scripts/EventBus/EventBus.cs
+++ Assets/Rc/Scripts/EventBus/EventBus.cs
@@ -3,32 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public interface IEvent { }
-
-public interface IEventReciver<T> where T : IEvent
+namespace rc
 {
-    void OnEventRecive(T evt);
-}
+    /// <summary>
+    /// 各使用者がこれを継承したイベントを作成します
+    /// </summary>
+    public interface IEvent { }
 
-public class EventBus<T> where T : IEvent
-{
-    static List<IEventReciver<T>> eventRecivers = new List<IEventReciver<T>>();
-
-    public void Notify(T evt)
+    /// <summary>
+    /// イベントを受け取るクラスがこれを継承してイベント受け取り関数を実装します
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEventReciver<T> where T : IEvent
     {
-        for (int i = 0; i < eventRecivers.Count; ++i)
+        void OnEventRecive(T evt);
+    }
+
+    /// <summary>
+    /// イベント登録されたときに static 領域が確保されて専用のバスが作成されます
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EventBus<T> where T : IEvent
+    {
+        static List<IEventReciver<T>> eventRecivers = new List<IEventReciver<T>>();
+
+        public void Notify(T evt)
         {
-            eventRecivers[i].OnEventRecive(evt);
+            for (int i = 0; i < eventRecivers.Count; ++i)
+            {
+                eventRecivers[i].OnEventRecive(evt);
+            }
         }
-    }
 
-    public void Subscribe(IEventReciver<T> eventReciver)
-    {
-        eventRecivers.Add(eventReciver);
-    }
+        public void Subscribe(IEventReciver<T> eventReciver)
+        {
+            eventRecivers.Add(eventReciver);
+        }
 
-    public void Unsubscribe(IEventReciver<T> eventReciver)
-    {
-        eventRecivers.Remove(eventReciver);
+        public void Unsubscribe(IEventReciver<T> eventReciver)
+        {
+            eventRecivers.Remove(eventReciver);
+        }
     }
 }
