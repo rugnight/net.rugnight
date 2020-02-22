f8eb08218d0fa7b5c640151916cc2de0221df5f0
rugnight <rugnight@gmail.com>
2020-02-22 11:42:02 +0900

    UnityからCMDを開く拡張追加

---
+++ Assets/Rc/Editor/OpenTerminal.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace rc
+{
+    public class OpenTerminal 
+    {
+        [MenuItem("Window/Run Terminal %t")]
+        static void RunTerminal()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            p.StartInfo.Arguments = "/k cd Assets";
+            p.Start();
+        }
+    }
+}
